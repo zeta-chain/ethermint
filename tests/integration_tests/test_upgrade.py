20cdd8d925c095dfd588038039e6197585e545b3
Francisco de Borja Aranda Castillejo <me@fbac.dev>
2024-07-26 13:50:58 +0200

    ignore python linting for commented test

--- tests/integration_tests/test_upgrade.py
+++ tests/integration_tests/test_upgrade.py
@@ -179,4 +179,4 @@
 #             home=cli.data_dir,
 #         )
 #     )
-#     assert p == {"allowed_clients": ["06-solomachine", "07-tendermint", "09-localhost"]}
+# assert p == {"allowed_clients": ["06-solomachine", "07-tendermint", "09-localhost"]}
